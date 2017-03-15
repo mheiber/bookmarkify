ac9ab5f1d5f75ed1a6f98e5304ac5541b231c9ab
Max Heiber <max.heiber@gmail.com>
2017-03-14 23:23:06 -0400

    support ES2015

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 #! /usr/bin/env node
 'use strict'
-const minify = require('minify')
+const minify = require('uglify-js').minify
 
-const file = process.argv[2]
-const label = process.argv[3]
-
-minify(file, (err, rawJS) => {
-    if (err) {
-        throw err
-    }
-    const wrappedJS = `!(function(){${rawJS}})()`
+const bookmarkify = (file, label) => {
+    const minified = minify(file)
+    const wrappedJS = `!(function(){${minified}})()`
     const href = `javascript:${escape(wrappedJS)}`
     const html = `<a href="${href}">${label}</a>`
+    return html
+}
+
+if (require.main === module) {
+    if (process.argv.length !== 4) {
+        throw new Error('expected bookmarkify <file> <label>')
+    }
+    const file = process.argv[2]
+    const label = process.argv[3]
+    const html = bookmarkify(file, label)
     console.log(html)
-})
+}
+
+module.exports = bookmarkify
 
